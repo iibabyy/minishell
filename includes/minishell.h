43b6cb8ecb765d64b75ffd24159e71f62364f934
Idrissa Baby <ibaby@made-f0Ar8s11.clusters.42paris.fr>
2024-07-24 19:31:11 +0200

    update

---
+++ includes/minishell.h
@@ -0,0 +1,19 @@
+#ifndef	MINISHELL_H
+#define MINISHELL_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/time.h>
+# include <sys/resource.h>
+# include <curses.h>
+# include <term.h>
+# include <dirent.h>
+
+#endif
---
+++ srcs/garbage_collector/garbage_collector.c
@@ -0,0 +1,3 @@
+#include "../../includes/minishell.h"
+
+void	ft_malloc()
