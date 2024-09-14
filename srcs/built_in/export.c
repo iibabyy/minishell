761a7886789d33582842c0e74e4617fdd1805db5
idrissa <utileidrissa@gmail.com>
2024-09-14 19:44:55 +0200

    save

--- srcs/built_in/export.c
+++ srcs/built_in/export.c
@@ -6,7 +6,7 @@
 /*   By: ibaby <ibaby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 15:19:09 by ibaby             #+#    #+#             */
-/*   Updated: 2024/09/11 22:27:11 by ibaby            ###   ########.fr       */
+/*   Updated: 2024/09/14 18:26:55 by ibaby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	export(char **args)
 	static t_env	*env = NULL;
 	t_env_var		*new_var;
 	int				i;
+	int				status;
 
 	if (env == NULL)
 		env = get_env_list(NULL);
@@ -27,17 +28,20 @@ int	export(char **args)
 		return (EXIT_SUCCESS);
 	if (args[1] == NULL)
 		return (print_export_var(env));
-	i = 0;
+	free((i = 0, status = EXIT_SUCCESS, NULL));
 	while (args[++i] != NULL)
 	{
 		if (parse_export_arg(args[i]) == EXIT_FAILURE)
+		{
+			status = EXIT_FAILURE;
 			continue ;
+		}
 		new_var = arg_to_env_var(args[i]);
 		if (new_var == NULL)
 			return (EXIT_FAILURE);
 		add_env_var(env, new_var);
 	}
-	return (EXIT_SUCCESS);
+	return (status);
 }
 
 int	print_export_var(t_env *env)
